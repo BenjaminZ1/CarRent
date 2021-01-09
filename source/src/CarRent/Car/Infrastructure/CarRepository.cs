7eb7dd0b0ca393f3fa5d82fd1388d52045b60b05
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-09 12:23:18 +0100

    implemented CarRepository

--- source/src/CarRent/Car/Infrastructure/CarRepository.cs
+++ source/src/CarRent/Car/Infrastructure/CarRepository.cs
@@ -2,10 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarRent.Car.Domain;
+using CarRent.Common.Domain;
 
 namespace CarRent.Car.Infrastructure
 {
-    public class CarRepository
+    
+    public class CarRepository : ICarRepository
     {
+        private readonly CarDbContext _db;
+
+        public CarRepository(CarDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Domain.Car> GetCar(int? id)
+        {
+            var car = new Domain.Car();
+            if (id != null)
+            {
+                car = await _db.Cars.FindAsync(id);
+            }
+            return car;
+        }
+
+        public IQueryable<Domain.Car> GetCars => _db.Cars;
+        public async Task<Response> Save(Domain.Car car)
+        {
+            Response response = new Response();
+            if (car.Id == 0)
+            {
+                try
+                {
+                    await _db.AddAsync(car);
+                    await _db.SaveChangesAsync();
+
+                    response.Id = car.Id;
+                    response.Flag = true;
+                    response.Message = "Has Been Added.";
+                }
+                catch (Exception e)
+                {
+                    response.Flag = false;
+                    response.Message = e.ToString();
+                }
+            }
+            else if (car.Id != 0)
+            {
+                Domain.Car entity = await GetCar(car.Id);
+                entity.Id = car.Id;
+                entity.Brand = car.Brand;
+                entity.Model = car.Model;
+                entity.Specification = car.Specification;
+
+                try
+                {
+                    await _db.SaveChangesAsync();
+                    response.Id = car.Id;
+                    response.Flag = true;
+                    response.Message= "Has Been Updated.";
+                }
+                catch (Exception e)
+                {
+                    response.Flag = false;
+                    response.Message = e.ToString();
+                }
+            }
+            return response;
+        }
+
+        public async Task<Response> DeleteAsync(int? id)
+        {
+            Response response = new Response();
+            Domain.Car car = await GetCar(id);
+
+            if (car != null)
+            {
+                try
+                {
+                    _db.Cars.Remove(car);
+                    await _db.SaveChangesAsync();
+
+                    response.Flag = true;
+                    response.Message = "Has been Deleted.";
+                }
+                catch (Exception e)
+                {
+                    response.Flag = false;
+                    response.Message = e.ToString();
+                }
+            }
+            else
+            {
+                response.Flag = false;
+                response.Message = "Car does not exist.";
+            }
+
+            return response;
+        }
     }
 }
