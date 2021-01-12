2a3d0bc5867513f64600016df725f4f56c609959
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-12 16:49:51 +0100

    added files for user and reservation

---
+++ source/src/CarRent/Reservation/Application/ReservationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.Reservation.Application
+{
+    public class ReservationDTO
+    {
+    }
+}
