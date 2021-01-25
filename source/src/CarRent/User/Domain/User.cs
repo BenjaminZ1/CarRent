688d01b841937a493a33f0a7ebee24e13cdc595a
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-25 20:00:42 +0100

    added user entity

--- source/src/CarRent/User/Domain/User.cs
+++ source/src/CarRent/User/Domain/User.cs
@@ -2,6 +2,11 @@
 {
     public class User
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string Place { get; set; }
+        public string PLZ { get; set; }
     }
 }
