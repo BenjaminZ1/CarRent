35c7186bcb0a84e15b5512d1f8655fcf849f0704
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-30 18:10:44 +0100

    implmented ReservationDto

--- source/src/CarRent/Reservation/Application/ReservationDTO.cs
+++ source/src/CarRent/Reservation/Application/ReservationDTO.cs
@@ -1,6 +1,22 @@
-﻿namespace CarRent.Reservation.Application
+﻿using System;
+
+namespace CarRent.Reservation.Application
 {
-    public class ReservationDTO
+    public class ReservationDto
     {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double TotalDays { get; set; }
+        public decimal TotalFee { get; set; }
+
+        public ReservationDto(Domain.Reservation reservation)
+        {
+            Id = reservation.Id;
+            StartDate = reservation.StartDate;
+            EndDate = reservation.EndDate;
+            TotalDays = reservation.TotalDays();
+            TotalFee = reservation.TotalFee();
+        }
     }
 }
