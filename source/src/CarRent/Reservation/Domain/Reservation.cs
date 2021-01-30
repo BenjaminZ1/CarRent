cdcb01ed2851f9f448e16bf378ac8ba38bd08975
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-30 18:07:36 +0100

    implmented Reservation

--- source/src/CarRent/Reservation/Domain/Reservation.cs
+++ source/src/CarRent/Reservation/Domain/Reservation.cs
@@ -1,6 +1,23 @@
-﻿namespace CarRent.Reservation.Domain
+﻿using System;
+
+namespace CarRent.Reservation.Domain
 {
     public class Reservation
     {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public virtual Car.Domain.Car Car { get; set; }
+
+
+        public double TotalDays()
+        {
+           return (EndDate - StartDate).TotalDays;
+        }
+
+        public decimal TotalFee()
+        {
+            return Car.Class.PricePerDay * (decimal)TotalDays();
+        }
     }
 }
