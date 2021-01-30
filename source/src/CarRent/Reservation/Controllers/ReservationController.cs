a50ef6b8b643982f2ad579ac3d235e4e87ddf8be
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-01-30 21:33:03 +0100

    implemented ReservationController

--- source/src/CarRent/Reservation/Controllers/ReservationController.cs
+++ source/src/CarRent/Reservation/Controllers/ReservationController.cs
@@ -1,12 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRent.Car.Application;
+using CarRent.Reservation.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarRent.Reservation.Controllers
 {
-    public class ReservationController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IReservationService _reservationService;
+
+        public ReservationController(IReservationService reservationService)
         {
-            return View();
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ReservationDto>> GetCar(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var data = await _reservationService.Get(id);
+                if (data == null)
+                    return NotFound();
+
+                return Ok(data);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReservationDto>>> GetCars()
+        {
+            try
+            {
+                var data = await _reservationService.GetAll();
+                if (data.Any())
+                    return Ok(data);
+
+                return NotFound();
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save([FromBody] Domain.Reservation reservation)
+        {
+            if (reservation == null)
+            {
+                return BadRequest();
+            }
+
+            var responseDto = await _reservationService.Save(reservation);
+            if (responseDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(responseDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var responseDto = await _reservationService.Delete(id);
+            if (responseDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(responseDto);
         }
     }
 }
